f643c90e6328edd440136874541b99deb1d30485
Keenan <keenanjj13@gmail.com>
2023-07-07 12:08:59 -0500

    feat: added telescope check

--- lua/telescope/_extensions/highlight-tweaker.lua
+++ lua/telescope/_extensions/highlight-tweaker.lua
@@ -1,5 +1,13 @@
-return require('telescope').register_extenesion {
+local has_telescope, telescope = pcall(require, 'telescope')
+
+if not has_telescope then
+  error('This plugin requires nvim-telescope/telescope.nvim (https://github.com/nvim-telescope/telescope.nvim)')
+end
+
+local list = require('telescope._extensions.highlight-tweaker').list
+
+return telescope.register_extenesion {
   exports = {
-    list = require('telescope-highlight-tweaker').list
+    list = list,
   }
 }
