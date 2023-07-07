048cb42beae8c93ac4bb2f81b313b1bea49d4044
Keenan <keenanjj13@gmail.com>
2023-07-07 12:41:31 -0500

    bug: fixed require

--- lua/telescope/_extensions/highlight-tweaker.lua
+++ lua/telescope/_extensions/highlight-tweaker.lua
@@ -4,7 +4,7 @@ if not has_telescope then
   error('This plugin requires nvim-telescope/telescope.nvim (https://github.com/nvim-telescope/telescope.nvim)')
 end
 
-local list = require('telescope._extensions.highlight-tweaker').list
+local list = require('telescope-highlight-tweaker').list
 
 return telescope.register_extension {
   exports = {
